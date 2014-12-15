90a2a3381ba199a483f3ca7f3f7ebd543c23bfb5
Roy Vegard Ovesen <rovo@statoil.com>
2014-12-15 12:46:57 +0100

    Initial

---
+++ FGM_160_Time_sync/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FGM_160_Time_sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
