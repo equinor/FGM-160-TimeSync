a8545647f5c49480856bc354f3d9a10609c2796e
Roy Vegard Ovesen <rovo@statoil.com>
2014-12-15 13:53:19 +0100

    Include Modbus library

--- FGM_160_Time_sync/Program.cs
+++ FGM_160_Time_sync/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Modbus.Device;
+
 
 namespace FGM_160_Time_sync
 {
@@ -9,6 +11,16 @@ namespace FGM_160_Time_sync
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("hello world");
+
+            // Read config file.
+
+            // Read clock from FGM160.
+
+            // Compare clock from FGM160 with local time.
+            // If difference is more than threshold then write
+            // local time to FGM160.
+
         }
     }
 }
